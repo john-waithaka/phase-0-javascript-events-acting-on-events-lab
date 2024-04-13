af678afe41853414a882c3f57a70e83f1ccf9c20
John Waithaka <johnwaruiwaithaka@gmail.com>
2024-04-13 09:26:44 +0300

    dodger right succcess

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Your code here
+// // Your code here
+
+const dodger = document.getElementById("dodger");
+
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+
+ //start of the right side dodger
+
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left < 180) {
+      dodger.style.left = `${left + 1}px`
+    }
+  }
+  
+
+ //end of the right hand side that is originally 40px wide
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
